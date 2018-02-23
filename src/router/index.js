71e0fdea03aa16dfc5e005376bac2b793e97071c
Luthfi <vlootfie@gmail.com>
2018-02-23 13:31:07 +0700

    fix conflict

--- src/router/index.js
+++ src/router/index.js
@@ -12,12 +12,6 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      meta: {},
-      path: '/',
-      name: 'RoomGame',
-      component: RoomGame
-    },
-    {
       path: '/arrow-generator',
       name: 'ArrowGenerator',
       component: ArrowGenerator
@@ -31,6 +25,12 @@ export default new Router({
       path: '/signin',
       name: 'SignIn',
       component: SignIn
+    },
+    {
+      meta: {},
+      path: '/',
+      name: 'RoomGame',
+      component: RoomGame
     }
   ]
 })
